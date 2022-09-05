2cf432203d2325627c69e0979996a016837cf176
sladuf <04fud27@gmail.com>
2022-09-05 20:56:16 +0900

    entity 만드는중

--- common/common/Entity.swift
+++ common/common/Entity.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+
+struct Weather: Codable {
+    let id: Int
+    let status: String
+    let icon: String
+    
+    enum CodingKeys: String, CodingKeys {
+        case list
+        case weather
+        case id
+        case description
+        case icon
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+//        let response = try values.nestedContainer(keyedBy: CodingKeys.self, forKey: .list)
+//        let weather = try values.nestedContainer(keyedBy: CodingKeys.self, forKey: .weather)
+        
+        self.id = try container.decode(Int.self, forKey: .id)
+        self.status = try container.decode(String.self, forKey: .description)
+        self.icon = try container.decode(String.self, forKey: .icon)
+        
+    }
+}
+
+struct ForecastWeather: Codable {
+    let weather: [Weather]
+    
+    enum CodingKeys: String, CodingKeys {
+        case list
+        case weather
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let response = try values.nestedContainer(keyedBy: CodingKeys.self, forKey: .list)
+        
+        let weather = try response.decode(Weather.self, forKey: .weather)
+        
+    }
+}
+
+Decoder.([Weather])
