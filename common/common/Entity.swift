896b922ea99c8b58affae04a85947ea97e4032e7
sladuf <04fud27@gmail.com>
2022-09-05 21:52:14 +0900

    entity property 추가

--- common/common/Entity.swift
+++ common/common/Entity.swift
@@ -11,23 +11,36 @@ struct Weather: Decodable {
     let id: Int
     let status: String
     let icon: String
+    let temp: Float
+    let minTemp: Float
+    let maxTemp: Float
+    let humidity: Int
 
     enum CodingKeys: String, CodingKey {
         case weather
+        case main
         case id
         case description
         case icon
+        case temp
+        case temp_min
+        case temp_max
+        case humidity
     }
 
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
-        let response = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .weather)
+        let weather = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .weather)
+        let main = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .main)
         
-        self.id = try response.decode(Int.self, forKey: .id)
-        self.status = try response.decode(String.self, forKey: .description)
-        self.icon = try response.decode(String.self, forKey: .icon)
-
+        self.id = try weather.decode(Int.self, forKey: .id)
+        self.status = try weather.decode(String.self, forKey: .description)
+        self.icon = try weather.decode(String.self, forKey: .icon)
+        self.temp = try main.decode(Float.self, forKey: .temp)
+        self.minTemp = try main.decode(Float.self, forKey: .temp_min)
+        self.maxTemp = try main.decode(Float.self, forKey: .temp_max)
+        self.humidity = try main.decode(Int.self, forKey: .humidity)
     }
 }
 
