a5af4bb03ac9187ff3ea2c813bf4ded2698b9892
sladuf <04fud27@gmail.com>
2022-09-05 21:42:02 +0900

    feat: weatherAPI

---
+++ common/common/WeatherAPI.swift
@@ -0,0 +1,49 @@
+//
+//  WeatherAPI.swift
+//  common
+//
+//  Created by 김민령 on 2022/09/05.
+//
+
+import Foundation
+import Moya
+
+enum WeatherAPI {
+    case today(lat: Float, lon: Float)
+    case forecast(lat: Float, lon: Float)
+}
+
+extension WeatherAPI : TargetType {
+    var baseURL: URL {
+        return URL(string: "https://api.openweathermap.org/data/2.5")!
+    }
+    
+    var path: String {
+        switch self {
+        case .today:
+            return "/weather"
+        case .forecast:
+            return "/forecast"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .today, .forecast:
+            return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .today(let lat, let lon), .forecast(let lat, let lon):
+            return .requestParameters(parameters: ["lat" : lat, "lon": lon, "appid": ""], encoding: URLEncoding.queryString)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return .none
+    }
+    
+    
+}
